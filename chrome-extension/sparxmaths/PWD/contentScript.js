4713918dfd894aa16e7b5ff03759fa00c9485797
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-18 10:13:10 -0700

    Create contentScript.js

---
+++ chrome-extension/sparxmaths/PWD/contentScript.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'continueScript') {
+    var s = document.createElement('script');
+    s.src = chrome.runtime.getURL('inject.js');
+    s.onload = function() {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
+    console.log('Password entered correctly! Continuing script...');
+  }
+});
