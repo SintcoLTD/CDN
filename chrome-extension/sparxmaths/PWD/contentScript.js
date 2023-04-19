820456f99a4157c7acab6c9d674a48670188e739
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-19 11:51:33 -0700

    Update contentScript.js

--- chrome-extension/sparxmaths/PWD/contentScript.js
+++ chrome-extension/sparxmaths/PWD/contentScript.js
@@ -1,11 +1,22 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'continueScript') {
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('inject.js');
-    s.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-    console.log('Password entered correctly! Continuing script...');
-  }
-});
+//START FILE CHECK FOR 1.1
+fetch('https://example.com/file.txt')
+  .then(response => response.text())
+  .then(text => {
+    if (text.includes('go')) {
+      /
+      /
+      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === 'continueScript') {
+          var s = document.createElement('script');
+          s.src = chrome.runtime.getURL('inject.js');
+          s.onload = function() {
+              this.remove();
+          };
+          (document.head || document.documentElement).appendChild(s);
+          console.log('Password entered correctly! Continuing script...');
+        }
+      });
+      // END PASSWORD CHECK
+    }
+  })
+  .catch(error => console.log(error));
